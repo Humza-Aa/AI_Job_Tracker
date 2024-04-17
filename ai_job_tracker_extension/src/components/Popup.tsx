bea8e58a87c7edabbf86ec933819fa795d2599fd
Humza-Aa <humzaaamir31@gmail.com>
2024-04-16 23:07:41 -0400

    Added: Xpath Queries

--- ai_job_tracker_extension/src/components/Popup.tsx
+++ ai_job_tracker_extension/src/components/Popup.tsx
@@ -1,13 +1,30 @@
 import { Box, Button, Flex, Heading } from "@chakra-ui/react";
 
 export default function Popup() {
+  // function getElementByXpath(path: string) {
+  //   return document.evaluate(
+  //     path,
+  //     document,
+  //     null,
+  //     XPathResult.FIRST_ORDERED_NODE_TYPE,
+  //     null
+  //   ).singleNodeValue;
+  // }
   const handleClick = async () => {
     let [tab] = await chrome.tabs.query({ active: true });
     chrome.scripting.executeScript({
       target: { tabId: tab.id! },
       func: () => {
-        
-      }
+        console.log(
+          document.evaluate(
+            "/html[1]/body[1]/div[4]/div[3]/div[4]/div[1]/div[1]/main[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/h2[1]/a[1]/span[1]",
+            document,
+            null,
+            XPathResult.FIRST_ORDERED_NODE_TYPE,
+            null
+          ).singleNodeValue
+        );
+      },
     });
   };
   return (
