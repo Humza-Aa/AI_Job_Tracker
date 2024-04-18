b663a3a6e533db2b650b9d45a24e5d9217a391d1
Humza-Aa <humzaaamir31@gmail.com>
2024-04-17 23:00:31 -0400

    Updated: Xpath to dynamic selector

--- ai_job_tracker_extension/src/components/Popup.tsx
+++ ai_job_tracker_extension/src/components/Popup.tsx
@@ -1,29 +1,70 @@
 import { Box, Button, Flex, Heading } from "@chakra-ui/react";
 
 export default function Popup() {
-  // function getElementByXpath(path: string) {
-  //   return document.evaluate(
-  //     path,
-  //     document,
-  //     null,
-  //     XPathResult.FIRST_ORDERED_NODE_TYPE,
-  //     null
-  //   ).singleNodeValue;
-  // }
   const handleClick = async () => {
-    let [tab] = await chrome.tabs.query({ active: true });
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
-      target: { tabId: tab.id! },
+      target: { tabId: tab.id!, allFrames: true },
       func: () => {
-        console.log(
-          document.evaluate(
-            "/html[1]/body[1]/div[4]/div[3]/div[4]/div[1]/div[1]/main[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/h2[1]/a[1]/span[1]",
+        type WindowDetails = {
+          [key: string]: HTMLElement | undefined;
+        };
+
+        // Initialize an empty object with the custom type
+        const windowDetails: WindowDetails = {};
+
+        interface Detail {
+          field: string;
+          Xpath: string;
+        }
+
+        const Details: Detail[] = [
+          {
+            field: "Position",
+            Xpath:
+              "/html[1]/body[1]/div[5]/div[3]/div[4]/div[1]/div[1]/main[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/h2[1]/a[1]/span[1]",
+          },
+          {
+            field: "Company",
+            Xpath:
+              "/html[1]/body[1]/div[5]/div[3]/div[4]/div[1]/div[1]/main[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/a[1]",
+          },
+          {
+            field: "Location",
+            Xpath:
+              "/html[1]/body[1]/div[5]/div[3]/div[4]/div[1]/div[1]/main[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[2]",
+          },
+          {
+            field: "JD",
+            Xpath:
+              "/html[1]/body[1]/div[5]/div[3]/div[4]/div[1]/div[1]/main[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[4]/article[1]/div[1]/div[1]/span[1]",
+          },
+        ];
+
+        Details.forEach((detail: Detail) => {
+          // Use document.evaluate to get the element using the XPath
+          const element = document.evaluate(
+            detail.Xpath,
             document,
             null,
             XPathResult.FIRST_ORDERED_NODE_TYPE,
             null
-          ).singleNodeValue
-        );
+          ).singleNodeValue as HTMLElement;
+
+          // If the element exists, assign it to the windowDetails object
+          if (element) {
+            windowDetails[detail.field] = element;
+          } else {
+            // Handle case where element is not found
+            console.error(`Element for field "${detail.field}" not found`);
+          }
+        });
+
+        // Access variables by their field names from windowDetails
+        console.log("Position:", windowDetails.Position?.innerText);
+        console.log("Company:", windowDetails.Company?.innerText);
+        console.log("Location:", windowDetails.Location?.innerText);
+        console.log("JD:", windowDetails.JD?.innerText);
       },
     });
   };
