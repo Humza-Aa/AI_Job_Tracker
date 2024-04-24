fa3cdc3ff0ba81c8bb88fc74a0b6c6e3ae97a4ca
Humza-Aa <humzaaamir31@gmail.com>
2024-04-23 21:31:16 -0400

    Added: Google Api for sheets

--- ai_job_tracker_backend/index.js
+++ ai_job_tracker_backend/index.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const { google } = require("googleapis");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", async (req, res) => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  const client = await auth.getClient();
+
+  const sheet = google.sheets({
+    version: "v4",
+    auth: client,
+  });
+
+  const spreadsheetId = process.env.SPREADSHEETID;
+
+  await sheet.spreadsheets.values.append({
+    spreadsheetId,
+    range: "Apr2024!A:I",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [],
+    },
+  });
+
+  res.send(getRows.data);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
