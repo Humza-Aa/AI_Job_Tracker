e7ca902b6370e200bec960dcac111c512696c21a
Humza-Aa <humzaaamir31@gmail.com>
2024-04-25 18:31:05 -0400

    Updated: Input fields

--- ai_job_tracker_backend/index.js
+++ ai_job_tracker_backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const { google } = require("googleapis");
+app.use(express.json());
+require('dotenv').config();
 
 app.get("/", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
@@ -18,16 +20,67 @@ app.get("/", async (req, res) => {
 
   const spreadsheetId = process.env.SPREADSHEETID;
 
-  await sheet.spreadsheets.values.append({
-    spreadsheetId,
-    range: "Apr2024!A:I",
-    valueInputOption: "USER_ENTERED",
-    resource: {
-      values: [],
-    },
-  });
+  const today = new Date();
+  const appliedDate = today.toISOString().split("T")[0];
 
-  res.send(getRows.data);
+  // Calculate deleteDeadline as today's date plus 1 month
+  const deleteDeadlineDate = new Date(today);
+  deleteDeadlineDate.setMonth(deleteDeadlineDate.getMonth() + 1);
+  const deleteDeadline = deleteDeadlineDate.toISOString().split("T")[0];
+
+  const {
+    positionTitle,
+    company,
+    location,
+    experienceLevel,
+    status,
+    preInterviewTasks,
+    jobDescription,
+    additionalInformation,
+  } = req.body;
+
+  const validExperienceLevels = ["Entry Level", "Mid Level", "Senior Level"];
+  const validStatusOptions = ["Applied", "Rejected", "Interviewing"];
+
+  if (
+    !validExperienceLevels.includes(experienceLevel) ||
+    !validStatusOptions.includes(status)
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Invalid experience level or status" });
+  }
+
+  const values = [
+    [
+      positionTitle,
+      company,
+      location,
+      experienceLevel,
+      appliedDate,
+      deleteDeadline,
+      status,
+      preInterviewTasks,
+      jobDescription,
+      additionalInformation,
+    ],
+  ];
+
+  try {
+    await sheet.spreadsheets.values.append({
+      spreadsheetId,
+      range: "Apr2024!A:I",
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values,
+      },
+    });
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Error appending to Google Sheet:", error);
+    res.sendStatus(500);
+  }
 });
 
 app.listen(port, () => {
